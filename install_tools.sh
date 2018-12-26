13c4a4b4989c3b4783bf472a48f62eb14f26978f
Stephen Hepner <shepner@asyla.org>
2018-12-25 20:30:24 -0600

    automatic update

--- install_tools.sh
+++ install_tools.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
-# WARNING!  This assumes it is being run from "~/Code/esp"
+# WARNING!  This assumes it is being run from "~/src/esp"
 
+WORKDIR=$HOME/src/esp
+mkdir -p $WORKDIR
 
 
 #setup toolchain
 #https://docs.espressif.com/projects/esp-idf/en/stable/get-started/macos-setup.html
 
 # This currently uses python2
-#sudo easy_install pip
-sudo pip install pyserial
+pip install pyserial
 
 
 # download and install the current stable ESP32 toolchain for macOS:
-URL=https://dl.espressif.com/dl/xtensa-esp32-elf-osx-1.22.0-80-g6c4433a-5.2.0.tar.g
-wget $URL
-tar -xzf `basename $URL`
+URL=https://dl.espressif.com/dl/xtensa-esp32-elf-osx-1.22.0-80-g6c4433a-5.2.0.tar.gz
+wget -N -P $WORKDIR $URL
+tar -xzf $WORKDIR/`basename $URL` -C $WORKDIR
 
 
 # update .profile as appropriate:
-cat >> ~/.profile <<'EOM'
+cat >> $HOME/.profile <<'EOM'
 
 # esp-idf
-if [ -d $HOME/Code/esp/xtensa-esp32-elf/bin ] ; then
-  export PATH=$PATH:$HOME/Code/esp/xtensa-esp32-elf/bin
-  export IDF_PATH=~/Code/esp/esp-idf
+if [ -d $HOME/src/esp/xtensa-esp32-elf/bin ] ; then
+  export PATH=$PATH:$HOME/src/esp/xtensa-esp32-elf/bin
+  export IDF_PATH=$HOME/src/esp/esp-idf
 fi
 
 EOM
@@ -32,7 +33,6 @@ EOM
 
 # https://docs.espressif.com/projects/esp-idf/en/stable/get-started/index.html#get-started-get-esp-idf
 # (re)install the ESP32 specific API/libraries
-rm -Rf esp-idf
-git clone -b v3.1.1 --recursive https://github.com/espressif/esp-idf.git
-
+rm -Rf $WORKDIR/esp-idf
+git clone -b v3.1.1 --recursive https://github.com/espressif/esp-idf.git $WORKDIR
 
--- update_git.sh
+++ update_git.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-git add *
+git pull origin master
+
+# git init
+git add .
+
+# this is needed only if there was a .git dir someplace it shouldnt have been
+#git rm --cached path_to_submodule
+# git rm --cached Freematics/ArduinoOBD
+
+git status
+
 git commit -m 'automatic update'
+#git remote add origin https://github.com/shepner/esp
+#git push --force -u origin master
 git push -u origin master
 
 
