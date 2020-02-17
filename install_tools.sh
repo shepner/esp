2d3069dabea8a2acb73e1a0cf7b11e53fa9c78c8
Stephen Hepner <shepner@asyla.org>
2020-02-16 20:08:47 -0600

    automatic update

--- install_tools.sh
+++ install_tools.sh
@@ -4,42 +4,52 @@
 WORKDIR=$HOME/src/esp
 mkdir -p $WORKDIR
 
-
+#install the XCode command line tools
+xcode-select --install
 
 #setup toolchain
 #https://docs.espressif.com/projects/esp-idf/en/stable/get-started/macos-setup.html
 
-pip install pyserial  # it currently uses python2
+
+#install CMake & Ninja build
+#brew install cmake ninja ccache
+brew upgrade cmake ninja ccache
 
 
 
-# download and install the current stable ESP32 toolchain for macOS:
-URL=https://dl.espressif.com/dl/xtensa-esp32-elf-osx-1.22.0-80-g6c4433a-5.2.0.tar.gz
-wget -N -P $WORKDIR $URL
-tar -xzf $WORKDIR/`basename $URL` -C $WORKDIR
+
+# (re)install the ESP32 specific API/libraries
+# https://docs.espressif.com/projects/esp-idf/en/stable/get-started/index.html#get-started-get-esp-idf
+VERSION="v4.0"
+
+rm -Rf $WORKDIR/esp-idf
+
+git clone -b $VERSION --recursive https://github.com/espressif/esp-idf.git  $WORKDIR/esp-idf-$VERSION
+
+ln -s $WORKDIR/esp-idf-$VERSION $WORKDIR/esp-idf
+
+
+
+
+# (re)install the ESP32 specific API/libraries
+$WORKDIR/esp-idf/install.sh
+
+
+
+
+# install the needed (python2) libs
+pip install --user -r /Users/shepner/src/esp/esp-idf/requirements.txt
+
+
 
 
 # update .profile as appropriate:
 cat >> $HOME/.profile <<'EOM'
 
 # esp-idf
-if [ -d $HOME/src/esp/xtensa-esp32-elf/bin ] ; then
-  export PATH=$PATH:$HOME/src/esp/xtensa-esp32-elf/bin
-  export IDF_PATH=$HOME/src/esp/esp-idf
+if [ -d $HOME/src/esp/esp-idf ] ; then
+  . $HOME/src/esp/esp-idf/export.sh
 fi
 
 EOM
-
-
-
-# (re)install the ESP32 specific API/libraries
-# https://docs.espressif.com/projects/esp-idf/en/stable/get-started/index.html#get-started-get-esp-idf
-VERSION="v3.2"
-
-rm -Rf $WORKDIR/esp-idf
-
-git clone -b $VERSION --recursive https://github.com/espressif/esp-idf.git esp-idf-$VERSION
-
-ln -s $WORKDIR/esp-idf-$VERSION $WORKDIR/esp-idf
-
 
